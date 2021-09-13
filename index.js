a3405aaa2a09e64400eeddd4edadd43ee364dffc
yatsun <yatsunlam25@gmail.com>
2021-09-13 00:39:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+function findMatching(drivers, string){
+    const names = drivers.filter(name => name.toUpperCase() === string.toUpperCase())
+    return names
+}
+
+function fuzzyMatch(drivers, string){
+    const names = drivers.filter(name => name.indexOf(string) === string.indexOf(string))
+    return names
+}
+
+function matchName(drivers, string){
+    return drivers.filter(driverName => driverName.name === string)
+}
